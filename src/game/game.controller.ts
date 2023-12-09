68817caa8fe32e2826a5467c0f4af75c45617628
Tadesse Alemayehu <tadessedev@gmail.com>
2023-12-09 10:33:46 +0300

    Update test for the get-games event message pattern

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -17,6 +17,7 @@ export class GameController implements OnModuleInit {
     @Inject('GAME-GATEWAY') private readonly gameClient: ClientKafka,
   ) {}
 
+  // TESTS for he bet controller of the game microservice
   @Get('/create-bet')
   createBet(@Body() createBetDto: CreateBetDto) {
     console.log('this is connecting');
@@ -120,11 +121,33 @@ export class GameController implements OnModuleInit {
       },
     });
   }
+
+  /// TEST the game controller of the game microservice
+
+  @Get('/get-games')
+  getGames() {
+    console.log('getting all games');
+    this.gameClient.send('get-games', '').subscribe({
+      next: (response) => {
+        // Handle successful response
+        // TODO: DO something with the result
+        console.log('Event published successfully:', response);
+      },
+      error: (error) => {
+        // Handle error
+        console.error('Error publishing event:', error);
+      },
+      complete: () => {
+        console.log('done emitting event ');
+      },
+    });
+  }
   async onModuleInit() {
     this.gameClient.subscribeToResponseOf('create-bet');
     this.gameClient.subscribeToResponseOf('get-recept-information');
     this.gameClient.subscribeToResponseOf('get-bet');
     this.gameClient.subscribeToResponseOf('redeem-bet');
+    this.gameClient.subscribeToResponseOf('get-games');
     await this.gameClient.connect();
   }
 }
