45ae5beaafc911760a4b4439c7612b1d4d1e0dd6
Tadesse Alemayehu <tadessedev@gmail.com>
2023-12-21 13:28:20 +0300

    Update daily, weekly, monthly filter for all log reporters

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,9 +1,8 @@
 import { Body, Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { CreateNotificationDto } from 'src/dto/create-notification.dto';
-import { ClientKafka, MessagePattern } from '@nestjs/microservices';
-import { Observable, Observer } from 'rxjs';
-import { on } from 'events';
+import { ClientKafka } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 @Controller('notification')
 // extend with the module init of the nestJs microservice
@@ -120,6 +119,7 @@ export class NotificationController implements OnModuleInit {
     );
     return await this.subscriber(observable);
   }
+
   // replicate the same method for git promotion logs, deposit logs, withdraw logs and event logs
   @Get('get-promotion-logs')
   async getPromotionLogs() {
@@ -130,6 +130,49 @@ export class NotificationController implements OnModuleInit {
     );
     return await this.subscriber(observable);
   }
+  // get-daily-promotion-logs
+  @Get('get-daily-promotion-logs')
+  async getDailyPromotionLogs() {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+    console.log(
+      'get-daily-promotion-logs form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-promotion-logs',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+  // get-weekly-promotion-logs
+  @Get('get-weekly-promotion-logs')
+  async getWeeklyPromotionLogs() {
+    const date = new Date();
+    date.setDate(date.getDate() - 7);
+    console.log(
+      'get-weekly-promotion-logs form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-promotion-logs',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+  // get-monthly-promotion-logs
+  @Get('get-monthly-promotion-logs')
+  async getMonthlyPromotionLogs() {
+    const date = new Date();
+    date.setDate(date.getDate() - 30);
+    console.log(
+      'get-monthly-promotion-logs form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-promotion-logs',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+
   @Get('get-deposit-logs')
   async getDepositLogs() {
     console.log('get-deposit-logs form notification controller gateway');
@@ -139,6 +182,45 @@ export class NotificationController implements OnModuleInit {
     );
     return await this.subscriber(observable);
   }
+  // get-daily-deposit-logs
+  @Get('get-daily-deposit-logs')
+  async getDailyDepositLogs() {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+    console.log('get-daily-deposit-logs form notification controller gateway');
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-deposit-logs',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+  // get-weekly-deposit-logs
+  @Get('get-weekly-deposit-logs')
+  async getWeeklyDepositLogs() {
+    const date = new Date();
+    date.setDate(date.getDate() - 7);
+    console.log('get-weekly-deposit-logs form notification controller gateway');
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-deposit-logs',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+  // get-monthly-deposit-logs
+  @Get('get-monthly-deposit-logs')
+  async getMonthlyDepositLogs() {
+    const date = new Date();
+    date.setDate(date.getDate() - 30);
+    console.log(
+      'get-monthly-deposit-logs form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-deposit-logs',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+
   @Get('get-withdraw-logs')
   async getWithdrawLogs() {
     console.log('get-withdraw-logs form notification controller gateway');
@@ -157,6 +239,7 @@ export class NotificationController implements OnModuleInit {
     );
     return await this.subscriber(observable);
   }
+
   @Get('get-failed-notifications')
   async getFailedNotifications() {
     console.log(
@@ -168,6 +251,50 @@ export class NotificationController implements OnModuleInit {
     );
     return await this.subscriber(observable);
   }
+  // get-daily-failed-notifications
+  @Get('get-daily-failed-notifications')
+  async getDailyFailedNotifications() {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+    console.log(
+      'get-daily-failed-notifications form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-failed-notifications',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+  // get-weekly-failed-notifications
+  @Get('get-weekly-failed-notifications')
+  async getWeeklyFailedNotifications() {
+    const date = new Date();
+    date.setDate(date.getDate() - 7);
+    console.log(
+      'get-weekly-failed-notifications form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-failed-notifications',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+
+  // get-monthly-failed-notifications
+  @Get('get-monthly-failed-notifications')
+  async getMonthlyFailedNotifications() {
+    const date = new Date();
+    date.setDate(date.getDate() - 30);
+    console.log(
+      'get-monthly-failed-notifications form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-failed-notifications',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+
   @Get('get-success-notifications')
   async getSuccessNotifications() {
     console.log(
@@ -176,6 +303,50 @@ export class NotificationController implements OnModuleInit {
     const observable: Observable<any> = this.notificationClient.send(
       'get-success-notifications',
       '',
+    );
+    return await this.subscriber(observable);
+  }
+
+  // get-daily-success-notifications
+  @Get('get-daily-success-notifications')
+  async getDailySuccessNotifications() {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+    console.log(
+      'get-daily-success-notifications form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-success-notifications',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+  // get-weekly-success-notifications
+  @Get('get-weekly-success-notifications')
+  async getWeeklySuccessNotifications() {
+    const date = new Date();
+    date.setDate(date.getDate() - 7);
+    console.log(
+      'get-weekly-success-notifications form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-success-notifications',
+      { from: date },
+    );
+    return await this.subscriber(observable);
+  }
+
+  // get-monthly-success-notifications
+  @Get('get-monthly-success-notifications')
+  async getMonthlySuccessNotifications() {
+    const date = new Date();
+    date.setDate(date.getDate() - 30);
+    console.log(
+      'get-monthly-success-notifications form notification controller gateway',
+    );
+    const observable: Observable<any> = this.notificationClient.send(
+      'get-success-notifications',
+      { from: date },
     );
     return await this.subscriber(observable);
   }
